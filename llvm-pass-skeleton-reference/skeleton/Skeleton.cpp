b9eb97a0ac0aea3a1c3aa060eb87fbeb00ab2189
Tal Derei <talderei99@gmail.com>
2023-06-05 07:48:34 -0700

    added simple binary operator pass

--- llvm-pass-skeleton-reference/skeleton/Skeleton.cpp
+++ llvm-pass-skeleton-reference/skeleton/Skeleton.cpp
@@ -6,8 +6,11 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 using namespace llvm;
 
+// LLVM function pass
 namespace {
     struct FunctionNamePass : public FunctionPass {
         static char ID;
@@ -15,7 +18,32 @@ namespace {
 
         virtual bool runOnFunction(Function &F) {
             errs() << "[*] function '" << F.getName() << "'\n";
-            return false;
+
+            // Iterate over basic blocks
+            for (auto &B : F) {
+                // Iterate over instructions
+                for (auto &I : B) {
+                    if (auto* op = dyn_cast<BinaryOperator>(&I)) { // check if binary operator
+                        errs() << "  [+] binary operator: " << op->getOpcodeName() << "\n";
+                        // Uniform API for creating instructions and inserting them into a basic block,
+                        // in this case where the 'op' instruction appears
+                        IRBuilder<> builder(op);
+
+                        // Make a multiply with the same operands as 'op'
+                        Value *lhs = op->getOperand(0);
+                        Value *rhs = op->getOperand(1);
+                        Value *mul = builder.CreateMul(lhs, rhs);
+
+                        // Swap out old instruction for new instruction for every instance of 'op'
+                        for (auto& U : op->uses()) {                        
+                            User *user = U.getUser();
+                            user->setOperand(U.getOperandNo(), mul);
+                        }
+                    }
+                }
+            }
+            // We modified the code 
+            return true;
         }
     };
 }
